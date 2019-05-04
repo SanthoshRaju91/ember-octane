affc9fab511f9f3ef6ec6e12170dfa3c8aa9866a
santhoshRaju91 <santhosh.codeholic@gmail.com>
2019-05-04 23:37:07 +0530

    workshop: Implementing route guards with ember route

--- tests/acceptance/login-test-test.js
+++ tests/acceptance/login-test-test.js
@@ -1,11 +1,18 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, fillIn, find, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import MockAuthService from '../stubs/auth-stub';
 
 module('Acceptance | login test', function(hooks) {
   setupApplicationTest(hooks);
 
+  hooks.beforeEach(function() {
+    this.owner.register("service:auth", MockAuthService);
+  })
+
   test('visiting /login and selecting the user and logging in the user', async function(assert) {
+    this.owner.lookup("service:auth").currentUserId = null;
+    
     await visit('/login');
     assert.equal(currentURL(), '/login');
     await fillIn("select", "1");
