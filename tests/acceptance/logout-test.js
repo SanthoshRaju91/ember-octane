876f446b294f6b68c7f0798345b50c1c5dcf40ba
santhoshRaju91 <santhosh.codeholic@gmail.com>
2019-05-05 02:10:31 +0530

    workshop: Adding nested subroutes

--- tests/acceptance/logout-test.js
+++ tests/acceptance/logout-test.js
@@ -13,9 +13,9 @@ module('Acceptance | logging out', function(hooks) {
   test('visiting /teams and clicking logout', async function(assert) {
     this.owner.lookup("service:auth").currentUserId = "1";
     
-    await visit('/teams');
+    await visit('/teams/linkedin');
     // await this.pauseTest();
-    assert.equal(currentURL(), "/teams");
+    assert.ok(currentURL().startsWith("/teams"));
   
     await click(".team-sidebar__logout-button");
     // await this.pauseTest();    
--- tests/integration/components/team-sidebar-test.js
+++ tests/integration/components/team-sidebar-test.js
@@ -10,8 +10,13 @@ module('Integration | Component | team-sidebar', function(hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<TeamSidebar />`);
-
+    const currentTeam = {
+      name: "LinkedIn",
+      order: 3,
+      iconUrl: ""
+    }
+    this.set("team", currentTeam);
+    await render(hbs`<TeamSidebar @team={{team}}/>`);    
     assert.deepEqual(this.element.textContent
         .trim()
         .replace(/\s*\n+\s*/g, "\n")
